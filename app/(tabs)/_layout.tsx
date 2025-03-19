05ed1c3a82d0ecf5b77661ddbc3989c71719e240
DylaneElyse <dylanecunningham@outlook.com>
2025-03-19 09:34:45 -0600

    Update tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,47 @@
 import { Tabs, Stack } from "expo-router";
 import React from "react";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+          if (route.name === "index") {
+            iconName = focused
+              ? require("../../assets/home-dark.png")
+              : require("../../assets/home-grey.png");
+          } else if (route.name === "search") {
+            iconName = focused
+              ? require("../../assets/search-dark.png")
+              : require("../../assets/search-grey.png");
+          } else if (route.name === "favorites") {
+            iconName = focused
+              ? require("../../assets/star-dark.png")
+              : require("../../assets/star-grey.png");
+          }
+          return <Image source={iconName} style={{ width: 24, height: 24 }} />;
+        },
+      })}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: () => (
-            <Image
-              source={require("../../assets/home-dark.png")}
-              style={{ width: 24, height: 24 }}
-            />
-          ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: "Search",
-          tabBarIcon: () => (
-            <Image
-              source={require("../../assets/search-dark.png")}
-              style={{ width: 24, height: 24 }}
-            />
-          ),
         }}
       />
       <Tabs.Screen
         name="favorites"
         options={{
           title: "Favorites",
-          tabBarIcon: () => (
-            <Image
-              source={require("../../assets/star-dark.png")}
-              style={{ width: 24, height: 24 }}
-            />
-          ),
         }}
       />
     </Tabs>
