e1e59288b574168640665496b6d03e5b49199cc4
DylaneElyse <dylanecunningham@outlook.com>
2025-03-26 12:01:17 -0600

    Update settings.tsx

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,31 +1,58 @@
 import React, { useContext } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import Header from "../../components/header";
 import { useAuth } from "../../contexts/AuthContext";
+import { useRouter } from "expo-router";
 
 export default function SettingsScreen() {
-  const { user } = useAuth(); // Get login state from context
+  const { user, signOut } = useAuth(); // Get login state from context
+  const router = useRouter();
+
   console.log(user);
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.replace("/LandingScreen"); // 🔥 Redirect to login after signing out
+  };
 
   if (!user) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Please log in to view favorites.</Text>
+      <View style={styles.pageContainer}>
+        <Header />
+        <View style={styles.container}>
+          <Text style={styles.text}>Please log in to view settings.</Text>
+        </View>
       </View>
     );
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.pageContainer}>
       <Header />
-      <Text style={styles.text}>Favorites Screen</Text>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 80 }}>
+          {/* Welcome, {username} */}
+        </Text>
+        <Text>Email: {user?.email}</Text>
+        <Text style={{ marginBottom: 200 }}>User ID: {user?.id}</Text>
+        <View style={{ marginTop: 50 }}></View>
+        <Button title="Sign Out" onPress={handleSignOut} color={"red"} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pageContainer: {
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#c2e8ff",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
     backgroundColor: "#c2e8ff",
     justifyContent: "center",
     alignItems: "center",
