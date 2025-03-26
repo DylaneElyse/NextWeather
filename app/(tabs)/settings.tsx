38e6f4335fa23750680c2d3df3de4d55dcc3f9b3
Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>
2025-03-26 14:09:18 -0600

    Update settings page
    Added button to log out, and the temp unit toggle. Can also view username and email here.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,40 @@
-import React, { useContext } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, TouchableOpacity, Image } from "react-native";
 import Header from "../../components/header";
 import { useAuth } from "../../contexts/AuthContext";
 import { useRouter } from "expo-router";
+import { supabase } from "../../lib/supabase";
+import { useTemperature } from "../../contexts/TemperatureContext";
 
 export default function SettingsScreen() {
+  const { temperatureUnit, temperatureUnitLetter, toggleTemperatureUnit } =
+    useTemperature();
   const { user, signOut } = useAuth(); // Get login state from context
+  const [username, setUsername] = useState("");
   const router = useRouter();
 
   console.log(user);
 
+    useEffect(() => {
+      const fetchUserProfile = async () => {
+        if (!user) return;
+  
+        const { data, error } = await supabase
+          .from("user_details") // Ensure this is the correct table name
+          .select("Username")
+          .eq("uuid", user.id) // Match the user ID
+          .maybeSingle();
+  
+        if (error) {
+          console.error("Error fetching user profile:", error);
+        } else {
+          setUsername(data?.Username);
+        }
+      };
+
+      fetchUserProfile();
+    }, [user]);
+  
   const handleSignOut = async () => {
     await signOut();
     router.replace("/LandingScreen"); // 🔥 Redirect to login after signing out
@@ -30,13 +55,47 @@ export default function SettingsScreen() {
     <View style={styles.pageContainer}>
       <Header />
       <View style={styles.container}>
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 80 }}>
-          {/* Welcome, {username} */}
-        </Text>
-        <Text>Email: {user?.email}</Text>
-        <Text style={{ marginBottom: 200 }}>User ID: {user?.id}</Text>
-        <View style={{ marginTop: 50 }}></View>
-        <Button title="Sign Out" onPress={handleSignOut} color={"red"} />
+        <Text style={styles.headerText}>Settings</Text>
+        <View style={styles.lineContainer}>
+          <Text style={styles.text}>Temperature Units</Text>
+                      <View style={{ justifyContent: "flex-end" }}>
+              <TouchableOpacity onPress={toggleTemperatureUnit}>
+                {temperatureUnit ? (
+                  <View style={styles.toggleContainer}>
+                    <Image
+                      source={require("../../assets/toggle/temperatureToggleC.png")}
+                      alt="Celsius"
+                      style={styles.toggleImage}
+                    />
+                    <Text style={styles.toggleText}>°C</Text>
+                  </View>
+                ) : (
+                  <View style={styles.toggleContainer}>
+                    <Image
+                      source={require("../../assets/toggle/temperatureToggleF.png")}
+                      alt="Fahrenheit"
+                      style={styles.toggleImage}
+                    />
+                    <Text style={styles.toggleText}>°F</Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+            </View>
+
+        </View>
+        <Text style={styles.headerText}>User Profile</Text>
+        <View style={styles.lineContainer}>
+          <Text style={styles.text}>Username:</Text>
+          <Text style={styles.text}>{username}</Text>
+        </View>
+        <View style={styles.lineContainer}>
+          <Text style={styles.text}>Email:</Text>
+          <Text style={styles.text}> {user?.email}</Text>
+        </View>
+        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+          <Text style={styles.buttonText}>Log Out</Text>
+        </TouchableOpacity>
+        
       </View>
     </View>
   );
@@ -56,8 +115,55 @@ const styles = StyleSheet.create({
     backgroundColor: "#c2e8ff",
     justifyContent: "center",
     alignItems: "center",
+    alignContent: "center",
+    marginTop: 150,
+  },
+  lineContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignContent: "center",
+    marginTop: 20,
+    width: 300,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 20,
   },
   text: {
-    color: "#fff",
+    fontSize: 20,
+    fontWeight: "semibold",
   },
+  button: {
+    width: 100,
+    height: 30,
+    backgroundColor: "#007AFF",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "semibold",
+  },
+  toggleImage: {
+    width: 35,
+    height: 35,
+  },
+  toggleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  toggleText: {
+    fontSize: 22,
+    fontWeight: "semibold",
+    width: 35,
+    textAlign: "center",
+  },
+
 });
