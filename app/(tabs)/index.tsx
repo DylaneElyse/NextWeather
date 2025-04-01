47b4ca51ba461f4721eb13bb7782340f111e87c6
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-04-01 13:10:17 -0600

    Reimplement temperature and hour offset for forecast of hours past 21h (9PM)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,13 +9,11 @@ import { useTemperature } from "../../contexts/TemperatureContext";
 import Header from "../../components/header";
 import { WEATHER_API_KEY } from "@env";
 import { formatMonthDate, formatDay, formatHour } from "../../components/getDayAndTime";
-import { militaryTimeToStandard } from "../../components/militaryTimeToStandard";
 
 export default function HomeScreen() {
   const { temperatureUnit, temperatureUnitLetter, toggleTemperatureUnit } = useTemperature();
   const [weatherData, setWeatherData] = React.useState<WeatherData | null>(null);
-  let { searchedCityLat, searchedCityLng } = useLocalSearchParams();
-  const [currentDate, setCurrentDate] = useState<string>("2000-01-01 00:00:00");
+  let { searchedCityLat, searchedCityLng, searchedCityName } = useLocalSearchParams();
 
   interface WeatherData {
     location?: {
@@ -60,7 +58,129 @@ export default function HomeScreen() {
     }
   }
 
+
+  // Hourly forecast for 20:00, 21:00, 22:00, 23:00 in Celsius
+
+  const [tempForecastC, setHourlyTempForecastC] = useState<number[]>([]);
+  const [hourForecastC, setHourlyTimeForecastC] = useState<number[]>([]);
+
+  const GetHourlyForecastCelsius = () => {
+    let currentHour = parseInt(formatHour(weatherData?.current?.last_updated ??  "1999-01-01 00:00"));
+      switch (currentHour) {
+        case 20:
+          setHourlyTempForecastC(
+            [weatherData?.forecast.forecastday[0].hour[21].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[22].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[23].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_c ?? -100
+          ]);
+          setHourlyTimeForecastC([21, 22, 23, 0]);
+            return;
+        case 21:
+          setHourlyTempForecastC(
+            [weatherData?.forecast.forecastday[0].hour[22].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[23].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_c ?? -100
+          ]);
+          setHourlyTimeForecastC([22, 23, 0, 1]);
+            return;
+        case 22:
+          setHourlyTempForecastC(
+            [weatherData?.forecast.forecastday[0].hour[23].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[2].temp_c ?? -100
+          ]);
+          setHourlyTimeForecastC([23, 0, 1, 2]);
+            return;
+        case 23:
+          setHourlyTempForecastC(
+            [weatherData?.forecast.forecastday[1].hour[0].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[2].temp_c ?? -100,
+            weatherData?.forecast.forecastday[1].hour[3].temp_c ?? -100
+          ]);
+          setHourlyTimeForecastC([0, 1, 2, 3]);
+            return;
+        default:
+          setHourlyTempForecastC(
+            [weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3].temp_c ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4].temp_c ?? -100,
+          ]);
+          setHourlyTimeForecastC([
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4,
+          ]);
+            return;
+  }
+};
   
+  // Hourly forecast for 20:00, 21:00, 22:00, 23:00 in Farenheit
+
+  const [tempForecastF, setHourlyTempForecastF] = useState<number[]>([]);
+  const [hourForecastF, setHourlyTimeForecastF] = useState<number[]>([]);
+
+  const GetHourlyForecastFarenheit = () => {
+    let currentHour = parseInt(formatHour(weatherData?.current?.last_updated ??  "1999-01-01 00:00"));
+      switch (currentHour) {
+        case 20:
+          setHourlyTempForecastF(
+            [weatherData?.forecast.forecastday[0].hour[21].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[22].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[23].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_f ?? -100
+          ]);
+          setHourlyTimeForecastF([21, 22, 23, 0]);
+            return;
+        case 21:
+          setHourlyTempForecastF(
+            [weatherData?.forecast.forecastday[0].hour[22].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[23].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_f ?? -100
+          ]);
+          setHourlyTimeForecastF([22, 23, 0, 1]);
+            return;
+        case 22:
+          setHourlyTempForecastF(
+            [weatherData?.forecast.forecastday[0].hour[23].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[0].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[2].temp_f ?? -100
+          ]);
+          setHourlyTimeForecastF([23, 0, 1, 2]);
+            return;
+        case 23:
+          setHourlyTempForecastF(
+            [weatherData?.forecast.forecastday[1].hour[0].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[1].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[2].temp_f ?? -100,
+            weatherData?.forecast.forecastday[1].hour[3].temp_f ?? -100
+          ]);
+          setHourlyTimeForecastF([0, 1, 2, 3]);
+            return;
+        default:
+          setHourlyTempForecastF(
+            [weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3].temp_f ?? -100,
+            weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4].temp_f ?? -100,
+          ]);
+          setHourlyTimeForecastF([
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3,
+            parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4,
+          ]);
+            return;
+  }
+};
+
   console.log("LAT/LONG \n", searchedCityLat, searchedCityLng);
 
   const fetchWeather = async (searchedCityLat: any, searchedCityLng: any) => {
@@ -79,7 +199,7 @@ export default function HomeScreen() {
       const data = await response.json();
       console.log("WEATHER DATA\n", data);
       setWeatherData(data);
-      setCurrentDate("2025-03-01 12:00");
+      await GetHourlyForecastCelsius();
       return data;
     } catch (error) {
       console.error(error);
@@ -88,6 +208,7 @@ export default function HomeScreen() {
 
   if (searchedCityLat == undefined){
     // Calgary as the default location
+    searchedCityName = "Calgary";
     searchedCityLat = "51.05";
     searchedCityLng = "-114.0667";
   }
@@ -95,9 +216,19 @@ export default function HomeScreen() {
   useEffect(() => {
     if (searchedCityLat && searchedCityLng) {
       fetchWeather(searchedCityLat, searchedCityLng);
+      GetHourlyForecastCelsius();
+      GetHourlyForecastFarenheit();
     }
   }, [searchedCityLat, searchedCityLng]);
   
+  useEffect(() => {
+    if (searchedCityLat && searchedCityLng) {
+      fetchWeather(searchedCityLat, searchedCityLng);
+      GetHourlyForecastCelsius();
+      GetHourlyForecastFarenheit();
+    }
+  }, []);
+
   return (
     <View style={styles.pageContainer}>
       <Header />
@@ -106,7 +237,7 @@ export default function HomeScreen() {
       <View style={styles.container}>
         <View style={{height: "15%", width: "100%", alignContent: "center", padding: 10,}}>
           <CityHeader
-            city={weatherData?.location?.name ?? "Unknown"}
+            city={Array.isArray(searchedCityName) ? searchedCityName.join(", ") : searchedCityName}
             temperature={weatherData?.current?.temp_c ?? -10}
             temperatureUnit={temperatureUnitLetter}
           />
@@ -128,23 +259,23 @@ export default function HomeScreen() {
 
         <View style={styles.containerCurrentWeather}>
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1].temp_c ?? -100}
+            hour={hourForecastC[0]}
+            temperature={tempForecastC[0]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2].temp_c ?? -100}
+            hour={hourForecastC[1]}
+            temperature={tempForecastC[1]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3].temp_c ?? -100}
+            hour={hourForecastC[2]}
+            temperature={tempForecastC[2]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4].temp_c ?? -100}
+            hour={hourForecastC[3]}
+            temperature={tempForecastC[3]}
             temperatureUnit={temperatureUnitLetter}
           />
         </View>
@@ -189,9 +320,11 @@ export default function HomeScreen() {
       :
       // Farenheit
       <View style={styles.container}>
+        
+        <View style={styles.container}>
         <View style={{height: "15%", width: "100%", alignContent: "center", padding: 10,}}>
           <CityHeader
-            city={weatherData?.location?.name ?? "Unknown"}
+            city={Array.isArray(searchedCityName) ? searchedCityName.join(", ") : searchedCityName}
             temperature={weatherData?.current?.temp_f ?? -10}
             temperatureUnit={temperatureUnitLetter}
           />
@@ -213,26 +346,64 @@ export default function HomeScreen() {
 
         <View style={styles.containerCurrentWeather}>
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 1].temp_f ?? -100}
+            hour={hourForecastF[0]}
+            temperature={tempForecastF[0]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 2].temp_f ?? -100}
+            hour={hourForecastF[1]}
+            temperature={tempForecastF[1]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 3].temp_f ?? -100}
+            hour={hourForecastF[2]}
+            temperature={tempForecastF[2]}
             temperatureUnit={temperatureUnitLetter}
           />
           <HourlyWeatherLabel
-            hour={parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4}
-            temperature={weatherData?.forecast.forecastday[0].hour[parseInt(formatHour(weatherData?.current?.last_updated ?? "1999-01-01 00:00")) + 4].temp_f ?? -100}
+            hour={hourForecastF[3]}
+            temperature={tempForecastF[3]}
             temperatureUnit={temperatureUnitLetter}
           />
         </View>
+
+        <View style={{height: "10%", width: "100%"}}>
+          <Text style={styles.header}>5 Days</Text>
+        </View>
+
+        <View style={styles.containerForecast}>
+          <ForecastLabel
+            date={formatMonthDate(weatherData?.forecast.forecastday[1].date ?? "1999-01-01 00:00:00")}
+            maxTemperature={weatherData?.forecast.forecastday[1].day.maxtemp_f ?? -100}
+            minTemperature={weatherData?.forecast.forecastday[1].day.mintemp_f ?? -100}
+            temperatureUnit={temperatureUnitLetter}
+          />
+          <ForecastLabel
+            date={formatMonthDate(weatherData?.forecast.forecastday[2].date ?? "1999-01-01 00:00:00")}
+            maxTemperature={weatherData?.forecast.forecastday[2].day.maxtemp_f ?? -100}
+            minTemperature={weatherData?.forecast.forecastday[2].day.mintemp_f ?? -100}
+            temperatureUnit={temperatureUnitLetter}
+          />
+          <ForecastLabel
+            date={formatMonthDate(weatherData?.forecast.forecastday[3].date ?? "1999-01-01 00:00:00")}
+            maxTemperature={weatherData?.forecast.forecastday[3].day.maxtemp_f ?? -100}
+            minTemperature={weatherData?.forecast.forecastday[3].day.mintemp_f ?? -100}
+            temperatureUnit={temperatureUnitLetter}
+          />
+          <ForecastLabel
+            date={formatMonthDate(weatherData?.forecast.forecastday[4].date ?? "1999-01-01 00:00:00")}
+            maxTemperature={weatherData?.forecast.forecastday[4].day.maxtemp_f ?? -100}
+            minTemperature={weatherData?.forecast.forecastday[4].day.mintemp_f ?? -100}
+            temperatureUnit={temperatureUnitLetter}
+          />
+           <ForecastLabel
+            date={formatMonthDate(weatherData?.forecast.forecastday[5].date ?? "1999-01-01 00:00:00")}
+            maxTemperature={weatherData?.forecast.forecastday[5].day.maxtemp_f ?? -100}
+            minTemperature={weatherData?.forecast.forecastday[5].day.mintemp_f ?? -100}
+            temperatureUnit={temperatureUnitLetter}
+          />
+        </View>
+      </View>
 
         <View style={{height: "10%", width: "100%"}}>
           <Text style={styles.header}>5 Days</Text>
